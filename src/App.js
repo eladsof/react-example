e983b889208355c228acc7ce7550f5f3e1b7fd8a
Elad Sofer <elad.sofer@gmail.com>
2020-07-15 12:12:30 +0300

    list with buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-var articles = [
-    {'title':'title1','content':'text1'},
-    {'title':'title2','content':'text2'},
-    {'title':'title3','content':'text3'}]
-;
+var isAdmin = false;
+
+class NewsList extends Component {
+
+    state = {
+        articles : [
+            {title:'title1',content:'text1'},
+            {title:'title2',content:'text2'},
+            {title:'title3',content:'text3'}
+        ]
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.articles.map(value =>
+                    <React.Fragment>
+                        <h3>{value.title}</h3>
+                        <h4>{value.content}</h4>
+                        {isAdmin && <button onClick={() => this.removeArticle(value)}>Remove</button>}
+                    </React.Fragment>)}
+            </div>
+        )
+    }
+
+    removeArticle(value) {
+        let index = this.state.articles.indexOf(value)
+        console.log(value + "is in index" + index);
+        console.log(this.state.articles);
+        this.state.articles.splice(index,1);
+        this.setState(this.state.articles)
+    }
+}
 
 function App() {
-  return (
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <h1>News!!!</h1>
-      </header>
-      <div>
-        {articles.map(value =>
-            <React.Fragment>
-              <h3>{value.title}</h3>
-              <h4>{value.content}</h4>
-            </React.Fragment>)}
-      </div>
+        <header className="App-header">
+            <h1>News!!!</h1>
+        </header>
+        <NewsList/>
     </div>
   );
 }
